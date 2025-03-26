0c726eb321152e6367325cc34ed7aa930bbb90fb
prathap024-ctrl <prathaplokesh04@gmail.com>
2025-03-26 10:02:33 +0530

    modified

--- app/compo/ProjectSec.tsx
+++ app/compo/ProjectSec.tsx
@@ -27,19 +27,6 @@ const ProjectSec = () => {
           </div>
           <div className="transform transition duration-300 hover:scale-110 rounded-lg dark:shadow-white shadow-sm h-52 w-56 hover:shadow-md bg-settheme flex items-center justify-center">
             <div className="px-5 pt-2 flex flex-col items-center">
-              <h2 className="font-semibold">AI Career Counselling</h2>
-              <Link href="">
-                <Button
-                  className="bg-blue-500 cursor-pointer text-settheme px-2 py-1 mt-2 rounded-md transition duration-150 hover:bg-blue-700"
-                  type="button"
-                >
-                  View Project
-                </Button>
-              </Link>
-            </div>
-          </div>
-          <div className="transform transition duration-300 hover:scale-110 rounded-lg dark:shadow-white shadow-sm h-52 w-56 hover:shadow-md bg-settheme flex items-center justify-center">
-            <div className="px-5 pt-2 flex flex-col items-center">
               <h2 className="font-semibold">Payments App</h2>
               <Link href="https://paytm-clone-black.vercel.app/">
                 <Button
